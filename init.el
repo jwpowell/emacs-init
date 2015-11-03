8874dd324912b1263e1d46532fafb3e387561bfa
Jeremy Powell <jeremy.wayne.powell@gmail.com>
2015-11-03 17:31:05 -0600

    Adding my first init.el

---
+++ init.el
@@ -0,0 +1,40 @@
+(require 'package)
+
+;;;; Package Management
+
+(defvar my-packages '(better-defaults
+		      ;paredit
+		      idle-highlight-mode
+		      ido-ubiquitous
+		      ;find-file-in-project
+		      ;magit
+		      smex
+                      base16-theme))
+
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+
+(when (< emacs-major-version 24)
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+
+(package-initialize)
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+;;;; UI Configuration
+
+(setq inhibit-splash-screen t)
+(set-default-font "Inconsolata 11")
+
+(load-theme 'base16-ashes-dark t)
+
+;;;; C language configuration
+
+(setq c-default-style "linux"
+      c-basic-offset 4)
+
